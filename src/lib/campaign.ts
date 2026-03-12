7173b2e10c58902206b35e9c15b0c098770aa2ab
Jorge Altamirano <grisjaa@gmail.com>
2026-03-12 12:17:53 -0300

    fix: update campaign title and description text

--- src/lib/campaign.ts
+++ src/lib/campaign.ts
@@ -1,9 +1,9 @@
 export async function getCampaign() {
   // Datos de tu campaña
   return {
-    title: "Quiero mi Pc Gamer!!!",
+    title: "Quiero mi primer Pc Gamer!!!",
     description:
-      "Hola, soy Joaquín tengo 13 años, y estoy lanzando esta campaña porque mi gran meta es armar mi propia PC Gamer. Para mí, esta computadora no es solo para jugar; es la herramienta que necesito para potenciar mis estudios y, al mismo tiempo, disfrutar de mi pasión por los videojuegos. \n\n Con tu apoyo, puedo estar más cerca de contar con un equipo que me permita aprender, crear y conectar con amigos en cada partida. Cada aporte suma muchísimo, desde un cafecito hasta compartir este link. ¡Gracias por impulsarme a cumplir este sueño!",
+      "Hola, soy Joaquín tengo 13 años, y estoy lanzando esta campaña porque mi gran meta es armar mi primer PC Gamer. Para mí, esta computadora no es solo para jugar; es la herramienta que necesito para potenciar mis estudios y, al mismo tiempo, disfrutar de mi pasión por los videojuegos. \n\n Con tu apoyo, puedo estar más cerca de contar con un equipo que me permita aprender, crear y conectar con amigos en cada partida. Cada aporte suma muchísimo, desde un cafecito hasta compartir este link. ¡Gracias por impulsarme a cumplir este sueño!",
     // ACÁ PODÉS CAMBIAR EL MONTO:
     amount: 2500000,
     // ACÁ ESTÁ LA IMAGEN (leyendo de public/img/pc gamer.webp)
